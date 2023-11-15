5a65267ce7765a27ff1099afb357b0a5e430e294
RalphOkorie <okorieralphc@gmail.com>
2023-11-15 13:54:12 -0800

    quick fix

--- _printf.c
+++ _printf.c
@@ -38,7 +38,7 @@ int _printf(const char *format, ...)
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
-					       flags, width, precision, size);
+				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
@@ -63,25 +63,4 @@ void print_buffer(char buffer[], int *buff_ind)
 		write(1, &buffer[0], *buff_ind);
 
 	*buff_ind = 0;
-}
-
-int main() {
-    // Test cases
-    printf("OUTPUT: ");
-    int result = _printf("% ");
-    printf("\nRETURN VALUE: %d\n", result);
-
-    printf("OUTPUT: ");
-    result = _printf("%\0");
-    printf("\nRETURN VALUE: %d\n", result);
-
-    printf("OUTPUT: ");
-    result = _printf(NULL);
-    printf("\nRETURN VALUE: %d\n", result);
-
-    printf("OUTPUT: ");
-    result = _printf("%s", NULL);
-    printf("\nRETURN VALUE: %d\n", result);
-
-    return 0;
 }

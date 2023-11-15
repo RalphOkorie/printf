50255129a42c1346ccab905d8611fc35a221e426
RalphOkorie <okorieralphc@gmail.com>
2023-11-15 14:14:15 -0800

    fix

--- _printf.c
+++ _printf.c
@@ -7,60 +7,92 @@ void print_buffer(char buffer[], int *buff_ind);
  * @format: format.
  * Return: Printed chars.
  */
-int _printf(const char *format, ...)
-{
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
 
-	if (format == NULL)
-		return (-1);
+int _printf(const char *format, ...) {
+    if (format == NULL)
+    {
+        // Handle case when format is NULL
+        return -1;
+    }
 
-	va_start(list, format);
+    va_list args;
+    va_start(args, format);
 
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
+    int count = 0;  // To keep track of the number of characters printed
 
-	print_buffer(buffer, &buff_ind);
+    while (*format) {
+        if (*format == '%') {
+            format++;  // Move past the '%'
 
-	va_end(list);
+            // Handle the space after %
+            if (*format == ' ') {
+                return -1;  // Return -1 for this case
+            }
 
-	return (printed_chars);
+            // Handle the '\0' after %
+            if (*format == '\0') {
+                return -1;  // Return -1 for this case
+            }
+
+            // Handle the %s specifier
+            if (*format == 's') {
+                char *str = va_arg(args, char *);
+                if (str == NULL) {
+                    // Handle NULL string
+                    printf("(null)");
+                    count += 6;  // Update count for the (null) string
+                } else {
+                    // Print the string
+                    printf("%s", str);
+                    count += strlen(str);  // Update count for the printed string
+                }
+            } else if (*format == 'c') {
+                // Handle the %c specifier
+                char c = va_arg(args, int);  // char is promoted to int in va_arg
+                putchar(c);
+                count++;  // Update count for the printed character
+            } else if (*format == '%') {
+                // Handle the %% specifier
+                putchar('%');
+                count++;  // Update count for the printed '%'
+            } else {
+                // Handle unsupported specifier
+                putchar('%');
+                putchar(*format);
+                count += 2;  // Update count for the '%' and the unsupported character
+            }
+        } else {
+            // Print normal characters
+            putchar(*format);
+            count++;  // Update count for the printed character
+        }
+
+        format++;  // Move to the next character in the format string
+    }
+
+    va_end(args);
+
+    return count;  // Return the total number of characters printed
 }
 
-/**
- * print_buffer - It prints the contents of the buffer if it exist
- * @buffer: Buffer array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
+int main() {
+    // Test cases
+    printf("OUTPUT: ");
+    int result = _printf("% ");
+    printf("\nRETURN VALUE: %d\n", result);
 
-	*buff_ind = 0;
+    printf("OUTPUT: ");
+    result = _printf("%\0");
+    printf("\nRETURN VALUE: %d\n", result);
+
+    printf("OUTPUT: ");
+    result = _printf(NULL);
+    printf("\nRETURN VALUE: %d\n", result);
+
+    printf("OUTPUT: ");
+    result = _printf("%s", NULL);
+    printf("\nRETURN VALUE: %d\n", result);
+
+    return 0;
 }
+
